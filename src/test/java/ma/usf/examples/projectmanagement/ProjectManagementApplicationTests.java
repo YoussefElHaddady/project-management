d627d5addd8a18081687506b1f604f9b1ab2e364
YouSsef <youssef.elhaddady@usmba.ac.ma>
2021-02-17 23:45:18 +0100

    project creation

---
+++ src/test/java/ma/usf/examples/projectmanagement/ProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.usf.examples.projectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
