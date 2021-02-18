b8ee94834e387d6ef27998de923443b8e06a5e36
YouSsef <youssef.elhaddady@usmba.ac.ma>
2021-02-18 23:05:13 +0100

    seeding the DB with CommandLineRunner

--- src/main/java/ma/usf/examples/projectmanagement/entities/Employee.java
+++ src/main/java/ma/usf/examples/projectmanagement/entities/Employee.java
@@ -13,23 +13,37 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
-@Data
 @Entity
+@Data
+@NoArgsConstructor
 public class Employee {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
+	@NonNull
 	private String firstName;
 
+	@NonNull
 	private String lastName;
 
+	@NonNull
 	private String email;
 
 	@ManyToMany(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH,
 			CascadeType.PERSIST }, fetch = FetchType.LAZY)
 	@JoinTable(name = "project_employee", joinColumns = @JoinColumn(name = "employee_id"), inverseJoinColumns = @JoinColumn(name = "project_id"))
 	private List<Project> projects;
+
+	public Employee(@NonNull String firstName, @NonNull String lastName, @NonNull String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
 }
--- src/main/java/ma/usf/examples/projectmanagement/entities/Project.java
+++ src/main/java/ma/usf/examples/projectmanagement/entities/Project.java
@@ -1,5 +1,6 @@
 package ma.usf.examples.projectmanagement.entities;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -13,19 +14,25 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
-@Data
 @Entity
+@Data
+@NoArgsConstructor
 public class Project {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
+	@NonNull
 	private String name;
 
+	@NonNull
 	private String stage; // NOTSTARTED, COMPLETED, INPROGRESS
 
+	@NonNull
 	private String description;
 
 //	@OneToMany(mappedBy = "project")
@@ -33,4 +40,20 @@ public class Project {
 			CascadeType.PERSIST }, fetch = FetchType.LAZY)
 	@JoinTable(name = "project_employee", joinColumns = @JoinColumn(name = "project_id"), inverseJoinColumns = @JoinColumn(name = "employee_id"))
 	private List<Employee> employees;
+
+	public Project(@NonNull String name, @NonNull String stage, @NonNull String description) {
+		super();
+		this.name = name;
+		this.stage = stage;
+		this.description = description;
+	}
+
+	public void addEmployee(Employee emp) {
+		if(employees == null) {
+			employees = new ArrayList<>();
+		}
+		
+		employees.add(emp);
+	}
+
 }
