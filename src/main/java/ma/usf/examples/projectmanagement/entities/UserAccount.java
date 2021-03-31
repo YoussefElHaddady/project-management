7517f56d19bf6729a0fec6c11e4e103c4ab75ab7
yelhaddady <yelhaddady@novelis.io>
2021-03-31 17:07:09 +0100

    Spring Security : Postgres Database with JDBC Authorization Authentication

---
+++ src/main/java/ma/usf/examples/projectmanagement/entities/UserAccount.java
@@ -0,0 +1,35 @@
+package ma.usf.examples.projectmanagement.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_accounts")
+@Data
+@NoArgsConstructor
+public class UserAccount {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_accounts_seq")
+	@Column(name = "user_id")
+	private long userId;
+
+	@Column(name = "username")
+	private String userName;
+
+	private String email;
+
+	private String password;
+
+	private String role;
+
+	private boolean enabled = true;
+
+}
--- src/main/java/ma/usf/examples/projectmanagement/security/SecurityConfiguration.java
+++ src/main/java/ma/usf/examples/projectmanagement/security/SecurityConfiguration.java
@@ -4,47 +4,43 @@ package ma.usf.examples.projectmanagement.security;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	DataSource dataSource; // automatically wired to H2 DB
+	DataSource dataSource; // automatically wired to our DB
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptEncoder;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.jdbcAuthentication().dataSource(dataSource)
-			.withDefaultSchema()
-			.withUser("admin").password("admin").roles("ADMIN")
-			.and().withUser("myuser").password("pass").roles("USER")
-			.and().withUser("otheruser").password("opass").roles("USER");
+			.usersByUsernameQuery("SELECT username, password, enabled "
+					+ "from user_accounts where username = ?")
+			.authoritiesByUsernameQuery("select username, role "
+					+ "from user_accounts where username = ?")
+			.passwordEncoder(bCryptEncoder);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 			.antMatchers("/projects/new").hasRole("ADMIN")
+			.antMatchers("/projects/save").hasRole("ADMIN")
 			.antMatchers("/employees/new").hasRole("ADMIN")
-			.antMatchers("/h2-console/**").permitAll()
-			.antMatchers("/").authenticated()
-			.and().formLogin();
-		
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
-	}
-
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+			.antMatchers("/employees/save").hasRole("ADMIN")
+			.antMatchers("/", "/**").permitAll()
+			.and()
+			.formLogin();
 	}
 
 }
