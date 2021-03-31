b1387dfccc990c34a6a29e1780dd2ee17ad9cf2a
yelhaddady <yelhaddady@novelis.io>
2021-03-31 15:48:30 +0100

    Spring Security : Basic In-Memory Authorization

--- src/main/java/ma/usf/examples/projectmanagement/security/SecurityConfiguration.java
+++ src/main/java/ma/usf/examples/projectmanagement/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package ma.usf.examples.projectmanagement.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -14,12 +15,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-		.withUser("myuser").password("pass").roles("USER")
-		.and()
-		.withUser("otheruser").password("opass").roles("USER");
+		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN").and().withUser("myuser")
+				.password("pass").roles("USER").and().withUser("otheruser").password("opass").roles("USER");
 	}
-	
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/projects/new").hasRole("ADMIN")
+			.antMatchers("/employees/new").hasRole("ADMIN")
+			.antMatchers("/").authenticated()
+			.and().formLogin();
+	}
+
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance();
